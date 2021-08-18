8e070af500a0d2e9b684c3954f5527ffb9fe46ad
Nur-Alom <alamn7150@gmail.com>
2021-08-18 13:32:27 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,37 @@
+
+
+// pin generator.
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if (pinString.length == 4) {
+        return pin;
+    }
+    else {
+        // console.log('got wrong number', pin);
+        return getPin();
+    }
+}
+
+function generatePin() {
+    const pin = getPin();
+    document.getElementById('display-pin').value = pin;
+}
+
+
+// key pad.
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = '';
+        }
+    }
+    else {
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+});
